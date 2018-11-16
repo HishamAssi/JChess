2aa62a159b66fb5d9b6d5bd057729d79574d2164
Hisham <hisham96assi@gmail.com>
2018-11-16 16:46:06 -0500

    Started work on Knight piece and implementing more basic structure of other classes along the way.

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,6 +1,30 @@
 package com.chess.engine.pieces;
 
-public class Piece {
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.List;
+
+public abstract class Piece {
+
+    protected final int piecePosition;
+
+    protected final Alliance pieceAlliance;
+
+    Piece(final int piecePosition, final Alliance pieceAlliance) {
+        this.piecePosition = piecePosition;
+        this.pieceAlliance = pieceAlliance;
+    }
+
+    public Alliance getPieceAlliance() {
+        return this.pieceAlliance;
+    }
+
+    /*
+    Returns a list of legal moves based on the current board.
+     */
+    public abstract List<Move> calculateLegalMoves(final Board board);
 
 
 
